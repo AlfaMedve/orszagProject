18ca603c00321caba860b0318d5898baba3a5ffa
Sajat Nev <email@pelda.hu>
2026-04-24 11:49:26 +0200

    mentes4

---
+++ js/adat2.js
@@ -0,0 +1,61 @@
+const japanTomb=[
+    {
+        "szokas":"Meghajlás (Ojigi)",
+        "mondat":"Japánban a tisztelet, az üdvözlés és a bocsánatkérés legfőbb kifejezőeszköze, melynek mélysége a helyzettől és a másik fél rangjától függ. A hétköznapi üdvözléstől a mély, 45 fokos bocsánatkérésig terjed, és fontos, hogy a mozdulatot derékból, egyenes háttal végezzék. A férfiak mellett tartott kézzel, a nők pedig maguk előtt összefogott kézzel hajolnak meg.",
+        "kep":""
+    },
+    {
+      "szokas":"Cipőlevétel (Genkan)",
+      "mondat":"Japánban a lakásokba, templomokba és bizonyos éttermekbe lépve kötelező levenni az utcai cipőt. A bejáratnál egy süllyesztett részen hagyják a lábbelit, és gyakran vendégpapucsot húznak helyette.",
+      "kep":""  
+    },
+    {
+        "szokas":"Tea-szertartás (Csado)",
+        "mondat":" Ez egy erősen ritualizált folyamat, amely a porított zöld tea (macsa) elkészítéséről és elfogyasztásáról szól. A szertartás a nyugalomról, a tiszteletről és az esztétikai élményről szól, nem csupán az ivásról.",
+        "kep":""  
+    },
+    {
+        "szokas":"Ajándékozás (Okurimono)",
+        "mondat":"Japánban az ajándék csomagolása gyakran fontosabb, mint maga a tartalom. Külön hagyománya van az évközi ajándékozásnak (Ocsúgen és Oszeibo), amellyel a hálát fejezik ki az ismerősök és üzletfelek felé.",
+        "kep":""  
+    },
+    {
+        "szokas":"Ajándékozás (Okurimono)",
+        "mondat":"Japánban az ajándék csomagolása gyakran fontosabb, mint maga a tartalom. Külön hagyománya van az évközi ajándékozásnak (Ocsúgen és Oszeibo), amellyel a hálát fejezik ki az ismerősök és üzletfelek felé.",
+        "kep":""  
+    },
+    {
+        "szokas":"Fürdőkultúra (Oszon )",
+        "mondat":"A vulkanikus tevékenységnek köszönhetően a termálvizes fürdők látogatása nemzeti hobbi. A közösségi fürdőzésnek szigorú szabályai vannak, például a medencébe lépés előtt alaposan meg kell tisztálkodni.",
+        "kep":""  
+    },
+    {
+        "szokas":"Cseresznyevirág-nézés (Hanami)",
+        "mondat":"Tavasszal az emberek összegyűlnek a parkokban a virágzó fák alatt, hogy piknikezzenek és ünnepeljék az élet mulandóságát. Ez az időszak az új kezdet szimbóluma is, mivel ekkor indul a tanév és a pénzügyi év.",
+        "kep":""  
+    },
+    {
+        "szokas":"Itadakimaszu és Gokiszoszama",
+        "mondat":"Evés előtt az „itadakimaszu” (alázattal elfogadom) kifejezéssel mondanak köszönetet az ételért. Az étkezés végén a „gokiszoszama desita” elmondásával fejezik ki hálájukat a szakácsnak és az alapanyagoknak.",
+        "kep":""  
+    },
+    {
+        "szokas":"Zajos evés (Szürcsölés)",
+        "mondat":"A tésztaételek, például a ramen vagy a szoba fogyasztásakor a hangos szürcsölés az elismerés jele. Ez azt jelzi a szakácsnak, hogy az étel ízlik, ráadásul segít lehűteni a forró tésztát és fokozza az aromákat.",
+        "kep":""  
+    },
+    {
+        "szokas":"Szuvenír (Omijage )",
+        "mondat":"Amikor valaki utazni megy, szinte kötelező helyi specialitásokat (általában szépen csomagolt édességet) hoznia a kollégáinak és családtagjainak. Ez a gesztus segít fenntartani a jó kapcsolatokat és ellensúlyozza a távollét miatti kiesést a munkából.",
+        "kep":""  
+    },
+    {
+        "szokas":"Kimono és Jukata viselése",
+        "mondat":"Bár a mindennapokban a nyugati ruházat dominál, fesztiválokon, esküvőkön és diplomázáskor ma is sokan viselnek tradicionális öltözetet. A jukata a kimono könnyedebb, pamutból készült változata, amelyet főleg nyári ünnepségeken hordanak.",
+        "kep":""  
+    },
+    
+
+
+    
+]
